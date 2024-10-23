eb952afdb70d06849d2caa80cd90a370057d4fff
cestmariel <marielise32@yahoo.fr>
2024-10-23 13:45:34 +0200

    dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Budget;
 use App\Entity\Categorie;
+use App\Entity\Commentaire;
+use App\Entity\Difficulte;
+use App\Entity\Ingredient;
+use App\Entity\Recette;
+use App\Entity\Tag;
+use App\Entity\UniteDeMesure;
+use App\Entity\User;
+use App\Entity\Ustensile;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -44,5 +53,14 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Categorie', 'fas fa-list', Categorie::class);
+        yield MenuItem::linkTocrud('Budget', 'fas fa-list', Budget::class);
+        yield MenuItem::linkTocrud('Commentaire', 'fas fa-list', Commentaire::class);
+        yield MenuItem::linkTocrud('Difficulte', 'fas fa-list', Difficulte::class);
+        yield MenuItem::linkTocrud('Ingredient', 'fas fa-list', Ingredient::class);
+        yield MenuItem::linkTocrud('Recette', 'fas fa-list', Recette::class);
+        yield MenuItem::linkTocrud('Tag', 'fas fa-list', Tag::class);
+        yield MenuItem::linkTocrud('UniteDeMesure', 'fas fa-list', UniteDeMesure::class);
+        yield MenuItem::linkTocrud('User', 'fas fa-list', User::class);
+        yield MenuItem::linkTocrud('Ustensile', 'fas fa-list', Ustensile::class);
     }
 }
