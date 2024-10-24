f701ecdbab0177a8cd41d18b49605f169588d447
Alex12ay <wemadd@outlook.fr>
2024-10-24 14:21:10 +0200

    je pense que j'ai fini la navbar

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
-
+use App\Repository\BudgetRepository;
+use App\Repository\IngredientRepository;
+use App\Repository\SaisonRepository;
 use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,12 +14,18 @@ class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
 
-    public function index(CategorieRepository $cr): Response
+    public function index(CategorieRepository $cr, IngredientRepository $ing, SaisonRepository $sr, BudgetRepository $br): Response
     {
+        $saison = $sr->findAll();
+        $budget = $br->findAll();
+        $ingredient = $ing->findAll();
         $categorie = $cr->findAll();
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
             'categorie' => $categorie,
+            'saison' => $saison,
+            'budget' => $budget,
+            'ingredient' => $ingredient,
         ]);
     }
 }
--- src/Controller/RecettesController.php
+++ src/Controller/RecettesController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 
 use App\Repository\CategorieRepository;
-
+use App\Repository\BudgetRepository;
+use App\Repository\IngredientRepository;
+use App\Repository\SaisonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,13 +14,18 @@ use Symfony\Component\Routing\Attribute\Route;
 class RecettesController extends AbstractController
 {
     #[Route('/recettes', name: 'app_recettes')]
-    public function index(CategorieRepository $cr): Response
+    public function index(CategorieRepository $cr,IngredientRepository $ing, SaisonRepository $sr, BudgetRepository $br): Response
     { 
+        $saison = $sr->findAll();
+        $budget = $br->findAll();
+        $ingredient = $ing->findAll();
         $categorie = $cr->findAll();
         return $this->render('recettes/index.html.twig', [
             'controller_name' => 'RecettesController',
             'categorie' => $categorie,
+            'saison' => $saison,
+            'budget' => $budget,
+            'ingredient' => $ingredient,
         ]);
     }
 }
-
