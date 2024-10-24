dc774e479686d33cffeec926209b13ae20dd320b
cestmariel <marielise32@yahoo.fr>
2024-10-24 16:49:46 +0200

    fin de journée

--- src/Controller/RecettesController.php
+++ src/Controller/RecettesController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 
 use App\Repository\CategorieRepository;
-
 use App\Repository\CommentaireRepository;
 use App\Repository\RecetteRepository;
 use App\Repository\BudgetRepository;
@@ -18,7 +17,7 @@ class RecettesController extends AbstractController
 {
     #[Route('/recettes', name: 'app_recettes')]
 
-    public function index(RecetteRepository $rr, CommentaireRepository $cr, CategorieRepository $cr,IngredientRepository $ing, SaisonRepository $sr, BudgetRepository $br): Response
+    public function index(RecetteRepository $rr, CommentaireRepository $cor, CategorieRepository $cr,IngredientRepository $ing, SaisonRepository $sr, BudgetRepository $br): Response
     { 
         $recettes = $rr->findAll();
         $averageNotes = [];
@@ -29,7 +28,7 @@ class RecettesController extends AbstractController
 
         //affichage de la note
         foreach ($recettes as $recette) {
-            $commentaires = $cr->findBy(['recette' => $recette]);
+            $commentaires = $cor->findBy(['recette' => $recette]);
             $totalNotes = 0;
             $count = count($commentaires);
 
@@ -51,13 +50,37 @@ class RecettesController extends AbstractController
     }
 
     #[Route('/recettes/{id}', name: 'app_recettes_show')]
-    public function show(RecetteRepository $rr, $id): Response
+    public function show(RecetteRepository $rr, CommentaireRepository $cor, CategorieRepository $cr,IngredientRepository $ing, SaisonRepository $sr, BudgetRepository $br, $id): Response
     { 
         $oneRec = $rr->find($id);
         $recettes = $rr->findAll();
+        $saison = $sr->findAll();
+        $budget = $br->findAll();
+        $ingredient = $ing->findAll();
+        $categorie = $cr->findAll();
+        $averageNotes = [];
+
+        //affichage de la note
+        foreach ($recettes as $recette) {
+            $commentaires = $cor->findBy(['recette' => $recette]);
+            $totalNotes = 0;
+            $count = count($commentaires);
+
+            foreach ($commentaires as $commentaire) {
+                $totalNotes += (float)$commentaire->getNote();
+            }
+
+            $averageNotes[$recette->getId()] = $count > 0 ? $totalNotes / $count : null;
+        }
+
         return $this->render('recettes/show.html.twig', [
-            'categorie' => $oneRec,
-            'categories' => $recettes,
+            'recette' => $oneRec,
+            'recettes' => $recettes,
+            'categorie' => $categorie,
+            'saison' => $saison,
+            'budget' => $budget,
+            'ingredient' => $ingredient,
+            'averageNotes' => $averageNotes,
         ]);
     }
 }
