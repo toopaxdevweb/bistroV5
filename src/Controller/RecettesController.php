942ec9aee5d413589f5cd1fb6a37ddbe26bf7c67
cestmariel <marielise32@yahoo.fr>
2024-10-25 11:49:48 +0200

    vend show recette

--- src/Controller/RecettesController.php
+++ src/Controller/RecettesController.php
@@ -7,9 +7,10 @@ use App\Repository\CategorieRepository;
 use App\Repository\CommentaireRepository;
 use App\Repository\RecetteRepository;
 use App\Repository\BudgetRepository;
+use App\Repository\DifficulteRepository;
 use App\Repository\IngredientRepository;
-use App\Repository\RecetteRepository;
 use App\Repository\SaisonRepository;
+use App\Repository\UstensileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -72,19 +73,48 @@ class RecettesController extends AbstractController
             'budgetName' => $budgetName,
             'budgetId' => $budgetId,
             'recette' => $recette,
-            'targetRecette' => $targetRecette,
-
+            'targetRecette' => $targetRecette,]);
+        }
 
     #[Route('/recettes/{id}', name: 'app_recettes_show')]
-    public function show(RecetteRepository $rr, CommentaireRepository $cor, CategorieRepository $cr,IngredientRepository $ing, SaisonRepository $sr, BudgetRepository $br, $id): Response
+    public function show(RecetteRepository $rr, CommentaireRepository $cor, CategorieRepository $cr,IngredientRepository $ing, SaisonRepository $sr, BudgetRepository $br, DifficulteRepository $dr, UstensileRepository $ur, $id): Response
     { 
         $oneRec = $rr->find($id);
+            if (!$oneRec) {
+                throw $this->createNotFoundException('Recette non trouvée');
+            }
         $recettes = $rr->findAll();
         $saison = $sr->findAll();
         $budget = $br->findAll();
         $ingredient = $ing->findAll();
         $categorie = $cr->findAll();
         $averageNotes = [];
+        $difficulte = $dr->findAll();
+        $ustensile = $ur->findAll();
+
+        // Récupérer les catégories de la recette actuelle
+        $categories = $oneRec->getCategorie();
+
+        // Trouver les recettes qui partagent au moins une catégorie
+        $troisRecettes = $rr->createQueryBuilder('r')
+        ->join('r.categorie', 'c')
+        ->where('c IN (:categories)')
+        ->andWhere('r.id != :currentId')
+        ->setParameter('categories', $categories)
+        ->setParameter('currentId', $id)
+        ->orderBy('r.date', 'DESC')
+        ->setMaxResults(3)
+        ->getQuery()
+        ->getResult();
+
+        // Récupérer tous les ingredients pour cette recette
+        $ingredients = $oneRec->getIngredient();
+
+        // Récupérer tous les ustensiles pour cette recette
+        $ustensiles = $oneRec->getUstensile();
+
+        // Récupérer tous les commentaires pour cette recette
+        $commentaires = $cor->findBy(['recette' => $oneRec], ['date' => 'DESC']);
 
         //affichage de la note
         foreach ($recettes as $recette) {
@@ -97,6 +127,7 @@ class RecettesController extends AbstractController
             }
 
             $averageNotes[$recette->getId()] = $count > 0 ? $totalNotes / $count : null;
+
         }
 
         return $this->render('recettes/show.html.twig', [
@@ -109,9 +140,13 @@ class RecettesController extends AbstractController
             'ingredient' => $ingredient,
             'averageNotes' => $averageNotes,
             'oneRec' => $oneRec,
-            'categories' => $recettes,
-
-
+            'categories' => $categories,
+            'difficulte' => $difficulte,
+            'troisRecettes' => $troisRecettes,
+            'commentaires' => $commentaires,
+            'ingredients' => $ingredients,
+            'ustensiles' => $ustensiles,
+            'ustensile' => $ustensile,
         ]);
     }
 }
